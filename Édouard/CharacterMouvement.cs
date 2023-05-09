96b138b75913902211b9b92245afdff43d044b0d
sasquade869-XXX_ <edcoucouc@gmail.com>
2023-05-09 19:48:11 -0400

    modification script movement

--- Édouard/CharacterMouvement.cs
+++ Édouard/CharacterMouvement.cs
@@ -1,3 +1,5 @@
+C#
+Copy code
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +7,8 @@ using UnityEngine;
 public class CharacterMouvement : MonoBehaviour
 {
     public float speed = 5f; // la vitesse du personnage
+    public float jumpForce = 7f; // la force du saut du personnage
+    public bool isGrounded; // variable qui stocke si le personnage touche le sol
 
     // fonction appelée à chaque image
     void Update()
@@ -17,5 +21,16 @@ public class CharacterMouvement : MonoBehaviour
 
         // déplace le personnage selon le vecteur de mouvement et la vitesse
         transform.position += movement * speed * Time.deltaTime;
+
+        // détecte si le personnage touche le sol
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.0f);
+
+        // si le joueur appuie sur la touche de saut et que le personnage touche le sol
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            // ajoute une force de saut au personnage
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
+
