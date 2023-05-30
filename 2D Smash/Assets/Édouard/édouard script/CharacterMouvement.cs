e67cd416a098c18b3fc10b6fd3c8b3e2acfa19eb
sasquade869-XXX_ <edcoucouc@gmail.com>
2023-05-30 18:46:36 -0400

    Something insaine!!!

---
+++ 2D Smash/Assets/Édouard/édouard script/CharacterMouvement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMouvement : MonoBehaviour
+{
+    public float moveSpeed;
+    public bool isJumping = false;
+
+    private bool isJumping;
+    private bool isGrounded;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+    private float horizontalMovement;
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapArea(GroundCheckLeft.position, groundCheckRight.position);
+
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+    }
+
+    void FixeUpdate()
+    {
+        MovePlayer(horizontalMovement);
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref targetVelocity, .05f);
+
+        if (isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+}
